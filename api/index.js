a66bd73773efca40997dbc365a6c67347aafb4fb
Wil <wilmerulcuevelasco@gmail.com>
2025-06-15 11:33:02 -0500

    ruta test de prueba

--- api/index.js
+++ api/index.js
@@ -42,3 +42,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.get('/test', (req, res) => {
+    res.status(200).json({ message: 'El servidor está funcionando correctamente!' });
+});
